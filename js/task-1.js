5624faeb4421837b46e4817e6e610d5516a8298f
dr-jeky11 <tyrsyna@gmail.com>
2024-04-27 18:37:48 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,8 @@ const menuItemsByClass = document.querySelectorAll(".item");
 const numberOfCategories = console.log(`Number of categories: ${menuItemsByClass.length}`);
 
 const showInfo = menuItemsByClass.forEach(item => {
+    const items = item.querySelectorAll("li")
     console.log(item.firstElementChild.textContent);
-    console.log(`Elements: ${item.lastElementChild}`);
+    console.log(`Elements: ${items.length}`);
 })
 
